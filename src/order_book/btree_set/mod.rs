18e66238ae0148e282beb0c3deb0f5fb30822d03
0xKitsune <0xKitsune@protonmail.com>
2023-05-30 19:31:13 -0400

    added tests for insert remove and update bid for btreeset

--- src/order_book/btree_set/mod.rs
+++ src/order_book/btree_set/mod.rs
@@ -11,8 +11,8 @@ use super::{
 };
 
 pub struct BTreeSetOrderBook {
-    bids: BTreeSet<Bid>,
-    asks: BTreeSet<Ask>,
+    pub bids: BTreeSet<Bid>,
+    pub asks: BTreeSet<Ask>,
 }
 
 impl BTreeSetOrderBook {
@@ -43,5 +43,144 @@ impl OrderBook for BTreeSetOrderBook {
         }
 
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{exchanges::Exchange, order_book::price_level::bid::Bid};
+
+    use super::BTreeSetOrderBook;
+
+    #[test]
+    fn test_insert_bid() {
+        let mut order_book = BTreeSetOrderBook::new();
+
+        let bid_0 = Bid::new(100.00, 50.0, Exchange::Binance);
+        let bid_1 = Bid::new(100.00, 50.0, Exchange::Bitstamp);
+        let bid_2 = Bid::new(101.00, 50.0, Exchange::Binance);
+        let bid_3 = Bid::new(101.00, 50.0, Exchange::Bitstamp);
+        let bid_4 = Bid::new(103.00, 50.0, Exchange::Binance);
+        let bid_5 = Bid::new(102.00, 50.0, Exchange::Binance);
+        let bid_6 = Bid::new(104.00, 50.0, Exchange::Binance);
+
+        // create an expected bids vector
+        let mut expected_bids = vec![
+            bid_0.clone(),
+            bid_1.clone(),
+            bid_2.clone(),
+            bid_3.clone(),
+            bid_4.clone(),
+            bid_5.clone(),
+            bid_6.clone(),
+        ];
+        // sort the vector because BTreeSet is ordered
+        expected_bids.sort();
+
+        order_book.bids.insert(bid_0);
+        order_book.bids.insert(bid_1);
+        order_book.bids.insert(bid_2);
+        order_book.bids.insert(bid_3);
+        order_book.bids.insert(bid_4);
+        order_book.bids.insert(bid_5);
+        order_book.bids.insert(bid_6);
+
+        // collect the actual bids from the BTreeSet into a vector
+        let actual_bids: Vec<Bid> = order_book.bids.iter().cloned().collect();
+
+        assert_eq!(actual_bids, expected_bids);
+    }
+
+    #[test]
+    fn test_remove_bid() {
+        let mut order_book = BTreeSetOrderBook::new();
+
+        let bid_0 = Bid::new(100.00, 50.0, Exchange::Binance);
+        let bid_1 = Bid::new(100.00, 50.0, Exchange::Bitstamp);
+        let bid_2 = Bid::new(101.00, 50.0, Exchange::Binance);
+        let bid_3 = Bid::new(101.00, 50.0, Exchange::Bitstamp);
+        let bid_4 = Bid::new(103.00, 50.0, Exchange::Binance);
+        let bid_5 = Bid::new(102.00, 50.0, Exchange::Binance);
+        let bid_6 = Bid::new(104.00, 50.0, Exchange::Binance);
+
+        // create an expected bids vector
+        let mut expected_bids = vec![
+            bid_0.clone(),
+            bid_1.clone(),
+            bid_2.clone(),
+            bid_3.clone(),
+            bid_4.clone(),
+        ];
+
+        // sort the vector because BTreeSet is ordered
+        expected_bids.sort();
+
+        order_book.bids.insert(bid_0);
+        order_book.bids.insert(bid_1.clone());
+        order_book.bids.insert(bid_2);
+        order_book.bids.insert(bid_3);
+        order_book.bids.insert(bid_4.clone());
+        order_book.bids.insert(bid_5);
+        order_book.bids.insert(bid_6.clone());
+
+        order_book.bids.remove(&bid_1);
+        order_book.bids.remove(&bid_4);
+        order_book.bids.remove(&bid_6);
+
+        // collect the actual bids from the BTreeSet into a vector
+        let actual_bids: Vec<Bid> = order_book.bids.iter().cloned().collect();
+
+        assert_eq!(actual_bids, expected_bids);
+    }
+
+    #[test]
+    fn test_update_bid() {
+        let mut order_book = BTreeSetOrderBook::new();
+
+        let bid_0 = Bid::new(100.00, 50.0, Exchange::Binance);
+        let bid_1 = Bid::new(100.00, 50.0, Exchange::Bitstamp);
+        let bid_2 = Bid::new(101.00, 50.0, Exchange::Binance);
+        let bid_3 = Bid::new(101.00, 499.0, Exchange::Bitstamp);
+        let bid_4 = Bid::new(103.00, 50.0, Exchange::Binance);
+        let bid_5 = Bid::new(102.00, 50.0, Exchange::Binance);
+        let bid_6 = Bid::new(104.00, 50.0, Exchange::Binance);
+
+        let replacement_bid_1 = Bid::new(100.00, 3404.0, Exchange::Bitstamp);
+        let replacement_bid_3 = Bid::new(101.00, 12309.0, Exchange::Binance);
+        let replacement_bid_6 = Bid::new(104.00, 20.0, Exchange::Bitstamp);
+
+        // create an expected bids vector
+        let mut expected_bids = vec![
+            bid_0.clone(),
+            replacement_bid_1.clone(),
+            bid_2.clone(),
+            replacement_bid_3.clone(),
+            bid_4.clone(),
+            bid_5.clone(),
+            replacement_bid_6.clone(),
+        ];
+
+        // sort the vector because BTreeSet is ordered
+        expected_bids.sort();
+
+        order_book.bids.insert(bid_0);
+        order_book.bids.insert(bid_1);
+        order_book.bids.insert(bid_2);
+        order_book.bids.insert(bid_3);
+        order_book.bids.insert(bid_4);
+        order_book.bids.insert(bid_5);
+        order_book.bids.insert(bid_6);
+
+        //insert the replacement bids
+        order_book.bids.insert(replacement_bid_6);
+        order_book.bids.insert(replacement_bid_3);
+        order_book.bids.insert(replacement_bid_1);
+
+        // collect the actual bids from the BTreeSet into a vector
+        let actual_bids: Vec<Bid> = order_book.bids.iter().cloned().collect();
+
+        assert_eq!(actual_bids, expected_bids);
+
+        dbg!("here");
     }
 }
